ce0c0bb1a7c9ca1e1483270eb6746c1bf530191d
HP814 <98508298+HP814@users.noreply.github.com>
2023-01-28 23:00:27 +0530

    Add files via upload

---
+++ js/blogh.js
@@ -0,0 +1,37 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+  
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
+
+
+
+
+
+
+const blogSection = document.querySelector('.blogs-section');
+
+db.collection("blogs").get().then((blogs) => {
+    blogs.forEach(blog => {
+        if(blog.id != decodeURI(location.pathname.split("/").pop())){
+            createBlog(blog);
+        }
+    })
+})
+
+const createBlog = (blog) => {
+    let data = blog.data();
+    blogSection.innerHTML += `
+    <div class="blog-card">
+        <img src="${data.bannerImage}" class="blog-image" alt="">
+        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
+        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+        <a href="/${blog.id}" class="btn dark">read</a>
+    </div>
+    `;
+}
